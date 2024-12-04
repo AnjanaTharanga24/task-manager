828ec551f16a77b3643b5183817e7c94f56d3a6b
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-12-04 23:12:40 +0530

    create delete task method

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -22,14 +22,28 @@ const getTaskById = async (req, res) => {
   try {
     const { id } = req.params;
     const task = await Task.findById(id);
-    res.status(200).json(task);
+   
     if(!task){
       return res.status(404).json(`no task with id ${id}`)
     }
+    res.status(200).send("Task deleted")
   } catch (error) {
     res.status(500).json({msg:error.message})
   }
  
 };
 
-module.exports = { createTask, getAllTasks, getTaskById };
+const deleteTaskById = async(req,res) =>{
+   try {
+    const {id} = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    res.status(200).json(task);
+    if(!task){
+      res.status(404).json({msg:error.message})
+    }
+   } catch (error) {
+     res.status(500).json({msg:error.message})
+   }
+}
+
+module.exports = { createTask, getAllTasks, getTaskById, deleteTaskById };
